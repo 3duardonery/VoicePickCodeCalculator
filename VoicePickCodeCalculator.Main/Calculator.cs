d92db4f2b6804d3fdee2a16df5a381e6e5342de9
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2020-11-01 15:33:31 -0300

    Initial Commit

---
+++ VoicePickCodeCalculator.Main/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using OperationResult;
+using System;
+using System.Text;
+
+namespace VoicePickCodeCalculator.Main
+{
+    public static class Calculator
+    {
+        public static Result<string> GetVoicePickCode(string gtin, string lote, string dataEmbalagem)
+        {
+            if (string.IsNullOrEmpty(gtin) || string.IsNullOrEmpty(lote) || string.IsNullOrEmpty(dataEmbalagem))
+                return Result.Error<string>(new Exception("Properties cannot be null or empty"));            
+                
+            var crc = Crc16.ComputeChecksum(Encoding.ASCII.GetBytes(
+                     string.Format("{0}{1}{2}", gtin, lote, dataEmbalagem)));
+
+            if (!crc.IsSuccess)
+                return Result.Error<string>(crc.Exception);
+
+            var resultVoicePick = string.Format("{0:0000}", crc.Value % 10000);
+
+            return Result.Success(resultVoicePick);
+        }
+    }
+}
---
+++ VoicePickCodeCalculator.Main/Crc16.cs
@@ -0,0 +1,60 @@
+﻿using OperationResult;
+using System;
+
+namespace VoicePickCodeCalculator.Main
+{
+    internal static class Crc16
+    {
+        private static ushort[] table = new ushort[256];
+        private const ushort polynominal = 0xA001;
+
+        static Crc16()
+        {
+            ushort value;
+            ushort temp;
+            for (ushort i = 0; i < table.Length; i++)
+            {
+                value = 0;
+                temp = i;
+                for (byte j = 0; j < 8; ++j)
+                {
+                    if (0 != ((value ^ temp) & 0x0001))
+                    {
+                        value = (ushort)((value >> 1) ^ polynominal);
+                    }
+                    else
+                    {
+                        value >>= 1;
+                    }
+                    temp >>= 1;
+                }
+                table[i] = value;
+            }
+        }
+
+        /// <summary>
+        /// Method responsable to calculate the voice pick code
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static Result<ushort> ComputeChecksum(byte[] bytes)
+        {
+            try
+            {
+                ushort crc = 0;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    byte index = (byte)(crc ^ bytes[i]);
+                    crc = (ushort)((crc >> 8) ^ table[index]);
+
+                }
+                return Result.Success(crc);
+            }
+            catch (Exception exc)
+            {
+                return Result.Error<ushort>(exc);
+            }
+        }
+
+    }
+}
